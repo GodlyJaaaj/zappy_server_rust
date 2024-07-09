26e2488c142c5d3115b42eb2c86cd22d823b9ddc
GodlyJaaaj <sebastien.lucas@epitech.eu>
2024-07-09 19:08:54 +0200

    feat: parse frequency

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -1,7 +1,7 @@
-use std::collections::HashSet;
-use clap::error::{Error};
-use clap::error::ErrorKind::{MissingRequiredArgument, ValueValidation};
+use clap::error::Error;
+use clap::error::ErrorKind::ValueValidation;
 use clap::Parser;
+use std::collections::HashSet;
 
 #[derive(Parser, Debug)]
 #[command(
@@ -12,27 +12,28 @@ use clap::Parser;
 )]
 pub struct ServerArgs {
     /// Server Port, must be in range 1-65535
-    #[arg(short, value_parser = parse_port)]
+    #[arg(short, value_parser = parse_port, required = true)]
     port: u16,
 
     /// Map Width
-    #[arg(short = 'x', value_parser = parse_width)]
+    #[arg(short = 'x', value_parser = parse_width, required = true)]
     width: u8,
 
     /// Map Height
-    #[arg(short = 'y', value_parser = parse_height)]
+    #[arg(short = 'y', value_parser = parse_height, required = true)]
     height: u8,
 
     /// Team Names
-    #[arg(short = 'n', long, num_args = 1..)]
+    #[arg(short = 'n', num_args = 1.., required = true)]
     teams: Vec<String>,
 
     /// Number of authorized client per team
-    #[arg(short = 'c', value_parser = parse_nb_clients)]
+    #[arg(short = 'c', value_parser = parse_nb_clients, required = true)]
     clients_nb: u64,
 
-    //Freq
-    // TODO: ... -f
+    /// Freq (1s/freq = MAX tps)
+    #[arg(short = 'f', value_parser = parse_freq, required = true)]
+    freq: u16,
 }
 
 pub fn parse_server_args() -> Result<ServerArgs, clap::Error> {
@@ -73,17 +74,16 @@ pub fn parse_port(s: &str) -> Result<u16, String> {
 fn validate_unique_teams(teams: &[String]) -> Result<(), clap::Error> {
     let mut unique_names = HashSet::new();
 
-    if teams.is_empty() {
-        return Err(Error::raw(MissingRequiredArgument, "Missing team names"));
-    }
     for name in teams {
         if !unique_names.insert(name.clone()) {
-            return Err(Error::raw(ValueValidation, format!("duplicates team name '{}'", name)));
+            return Err(Error::raw(
+                ValueValidation,
+                format!("duplicates team name '{}'", name),
+            ));
         }
     }
     Ok(())
 }
-
 
 pub fn parse_nb_clients(s: &str) -> Result<u64, String> {
     let clients_nb: u64 = s.parse().map_err(|_| "not a valid clients number")?;
@@ -93,4 +93,14 @@ pub fn parse_nb_clients(s: &str) -> Result<u64, String> {
     }
 
     Ok(clients_nb)
+}
+
+pub fn parse_freq(s: &str) -> Result<u16, String> {
+    let freq: u16 = s.parse().map_err(|_| "not a valid freq number")?;
+
+    if freq == 0 {
+        return Err(String::from("freq must greater than 0"));
+    }
+
+    Ok(freq)
 }
